41438991d658b1e0008bcb21361f38b510e19860
Lane araujo <laraujo@wpi.edu>
2023-07-13 10:26:02 -0400

    added dice

---
+++ Dice folder/Simple Dice Program/dice.js
@@ -0,0 +1,9 @@
+function roll(){
+    const sides = 6;
+    var randomNumber = Math.floor(Math.random() * sides) + 1;
+    let face = document.getElementById("face");
+    face.innerHTML = randomNumber;
+}
+
+var button = document.getElementById("button");
+button.addEventListener("click", roll);
